816920ec42a3143f73f7be358fb7fac00ccd0817
Mouadh Mkadmi <36245246+wvagen@users.noreply.github.com>
2020-01-01 21:52:54 +0100

    Version 2.0
    Scene1 Finished

---
+++ Assets/Scripts/Manager1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager1 : MonoBehaviour
+{
+    public Animator myAnim;
+
+    int sceneFrame = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            sceneFrame++;
+            myAnim.SetInteger("sceneFrame", sceneFrame);
+
+        }
+    }
+}
